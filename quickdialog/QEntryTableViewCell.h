283f580b00395795f7ffa70af761e7067fb1a550
Michael Ensly <michael@adapptor.com.au>
2013-09-20 14:53:03 +0800

    Safe unretained!

--- quickdialog/QEntryTableViewCell.h
+++ quickdialog/QEntryTableViewCell.h
@@ -30,7 +30,7 @@
     QTextField *_textField;
 
 @protected
-    __unsafe_unretained QuickDialogTableView *_quickformTableView;
+    __weak QuickDialogTableView *_quickformTableView;
 }
 
 @property(nonatomic, strong) QTextField *textField;
